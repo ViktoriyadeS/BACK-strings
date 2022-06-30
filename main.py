4623cac2a2cf9b3e989c0c170d040024f77f2e34
Viktoriya <81090951+ViktoriyadeS@users.noreply.github.com>
2022-06-30 13:22:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,22 +13,22 @@ player2 = "Marco van Basten"
 goal_0 = 32
 goal_1 = 54
 
-scorers = f"{player1} {goal_0}, {player2} {goal_1}"
+scorers = player1 + " " + str(goal_0)+", "+player2 + " " + str(goal_1)
 report = f"{player1} scored in the {goal_0}nd minute\n{player2} scored in the {goal_1}th minute"
+
 
 print(report)
 
 # Part 2
 
 player = "Marcel van Basten"
-first_name = player[:6]
-last_name = player[7:]
+first_name = player[:player.find(" ")]
+last_name = player[player.find(" ")+1:]
 last_name_len = len(last_name)
 
 name_short = f"{first_name[0]}. {last_name}"
 chant = (f"{first_name}! " * len(first_name)).rstrip(" ")
 good_chant = chant != chant.endswith(" ")
-
 
 
 print(good_chant)
